1fbf762418e52e287571d07d3f2fc515aef5d0c8
Devin Dreszer <devindreszer@gmail.com>
2014-12-14 13:42:46 -0700

    Hack to remove favicon error

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,11 +1,14 @@
 # rulers/lib/rulers.rb
 require "rulers/version"
-require "rulers/array"
 require "rulers/routing"
 
 module Rulers
   class Application
     def call(env)
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404, {'Content-Type' => 'text/html'}, []]
+      end
+
       klass, act = get_controller_and_action(env) #klass (class) >> PeopleController; act (action) >> create
       controller = klass.new(env)
       text = controller.send(act)
--- lib/rulers/routing.rb
+++ lib/rulers/routing.rb
@@ -2,7 +2,7 @@
 module Rulers
   class Application
     def get_controller_and_action(env)
-      _, cont, action, after = env["PATH_INFO"].split.('/', 4) # PATH_INFO = "/people/create/"
+      _, cont, action, after = env["PATH_INFO"].split('/', 4) # PATH_INFO = "/people/create/"
       cont = cont.capitalize # People
       cont += "Controller" # PeopleController
 
