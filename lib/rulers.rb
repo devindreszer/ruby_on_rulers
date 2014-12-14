5afbe1df9e3f117ad5061ef2b50fc991ea9f0531
Devin Dreszer <devindreszer@gmail.com>
2014-12-14 13:57:55 -0700

    Rulers can rescue errors with 500 page

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -11,7 +11,12 @@ module Rulers
 
       klass, act = get_controller_and_action(env) #klass (class) >> PeopleController; act (action) >> create
       controller = klass.new(env)
-      text = controller.send(act)
+      begin
+        text = controller.send(act)
+      rescue
+        return [500, {'Content-Type' => 'text/html'}, ["Internal Error"]]
+      end
+
       [200, {'Content-Type' => 'text/html'}, [text]]
     end
   end
